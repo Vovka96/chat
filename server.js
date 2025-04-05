08d70ba0af14d4558ae3fb00e8017a26d363f73e
Vovka96 <s.vovan.yu@gmail.com>
2025-04-05 15:00:45 +0300

    second less

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const { Server } = require("socket.io");
 
 const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
 const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
@@ -14,6 +15,21 @@ const server = http.createServer((req, res) => {
     }
     res.statusCode === 404;
     return res.end('Error 404');
+});
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected. id - ' + socket.id);
+    let userNickname = 'user';
+
+    socket.on('set_nickname', (nickname) => {
+        userNickname = nickname;
+    });
+
+    socket.on('new_message', (message) => {
+        io.emit('message', userNickname + ':' + message);
+    });
 });
 
 server.listen(3000);
